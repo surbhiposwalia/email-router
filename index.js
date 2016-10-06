86ad641468c740a32a94a05fc67d58036c0d3a26
surbhi <surbhi.poswalia@gmail.com>
2016-10-06 19:29:47 +0000

    trying email router from scratch

---
+++ index.js
@@ -0,0 +1,45 @@
+var React= require('react');
+var ReactDOM= require('react-dom');
+var router = require('react-router');
+var Router = router.Router;
+var Route = router.Route;
+var hashHistory = router.hashHistory;
+var Link= router.Link;
+var IndexRoute= router.IndexRoute;
+
+var EMAIL= {
+        inbox: {
+                0: {
+                    id: 0,
+                    from: "billg@microsoft.com",
+                    to: "TeamWoz@Woz.org",
+                    title: "Possible work opportunity",
+                    content: "Dear Woz.  Fancy a job at Mister Softee?  Bill x"
+                },
+                1: {
+                    id: 1,
+                    from: "zuck@facebook.com",
+                    to: "TeamWoz@Woz.org",
+                    title: "Do you know PHP?",
+                    content: "Dear Woz.  We are in need of a PHP expert.  Fast.  Zuck x"
+                }
+        },
+        spam: {
+                0: {
+                    id: 0,
+                    from: "ChEaPFl1ghTZ@hotmail.com",
+                    to: "TeamWoz@Woz.org",
+                    title: "WaNt CHEEp FlitZ",
+                    content: "Theyre CheEp"
+                },
+                1: {
+                    id: 1,
+                    from: "NiKEAIRJordanZ@hotmail.com",
+                    to: "TeamWoz@Woz.org",
+                    title: "JorDanz For SAle",
+                    content: "Theyre REELY CheEp"
+                }
+    }
+    
+};
+
